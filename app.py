c898c36af4933050a2f0c0aafe8101cefbde2a4e
Matthew Finch <finchrmatthew@gmail.com>
2021-08-02 16:57:48 -0400

    app finished

--- app.py
+++ app.py
@@ -1,13 +1,66 @@
 from flask import Flask, url_for
 from flask_sqlalchemy import SQLAlchemy
-from routes import Routes
+from flask import render_template, redirect, request
+from datetime import datetime
 
+def create_app(config_object='settings.config'):
+    app = Flask(__name__)
+    app.config.from_object(config_object)
+    return app
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app = Routes.initRoutes(app)
+app = create_app()
 db = SQLAlchemy(app)
 
+class TODO(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/', methods=["POST", "GET"])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = TODO(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an Error Adding your Task'
+    else:
+        tasks = TODO.query.order_by(TODO.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = TODO.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was an Error Deleting that task'
+
+@app.route('/update/<int:id>', methods=["GET", 'POST'])
+def update(id):
+    task = TODO.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an Error updating your task'
+
+    else:
+        return render_template('update.html', task=task)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ settings.py
@@ -0,0 +1,6 @@
+
+class config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    FLASK_APP = 'app.py'
+    FLASK_ENV = 'development'
